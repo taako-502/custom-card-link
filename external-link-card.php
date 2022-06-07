f90837a3906fcc7b0a0242a893bf84788d9359bb
taako-502 <i.am.armorknight@gmail.com>
2022-06-08 00:25:50 +0900

    外部リンクを追加する機能追加

--- external-link-card.php
+++ external-link-card.php
@@ -1,19 +1,28 @@
 <?php
+
+require_once __DIR__ .'/library/Get_OGP_InWP-main/get_ogp_inwp.php';
+
 /**
 * 外部リンクカード
 */
 function external_link_card_dynamic_render_callback($attributes) {
-	// var_dump($attr)
-	$url = isset($attributes['url']) ? $attributes['url'] : '';
+	$url         = isset($attributes['url']) ? $attributes['url'] : '';
+	$ogps        = \Get_OGP_InWP::get($url);
+	$image       = isset($ogps['og:image']) ? $ogps['og:image'] : '';
+	$title       = isset($ogps['og:title']) ? $ogps['og:title'] : '';
+	$description = isset($ogps['og:description']) ? $ogps['og:description'] : '';
 	return sprintf(
-		'<div>
+		'<div class="elc">
 			<a href="%s">
-				<img src="%s">
-				<p>Description</p>
+				<img class="elc--thumbnail" src="%s">
+				<p class="elc--title">%s</p>
+				<p class="elc--title">%s</p>
 			</a>
 		</div>',
 		$url,
-		'https://hepere.com/wp-content/uploads/2022/05/frontpage.jpeg'
+		$image,
+		$title,
+		$description
 	);
 }
 
