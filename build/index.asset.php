ff07da88caa26b892538b709b4ed96e1051384e7
taako-502 <i.am.armorknight@gmail.com>
2022-06-08 18:52:24 +0900

    PHPでレンダリングしたHTMLがEditorにも表示できるよう修正

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '91bc55070bab3c4bcabc');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => '658eb85cf77d0adcfe66');
--- src/edit.js
+++ src/edit.js
@@ -1,10 +1,16 @@
 import { TextControl } from '@wordpress/components';
+import ServerSideRender from '@wordpress/server-side-render';
 import { useBlockProps } from '@wordpress/block-editor';
+import metadata from './block.json';
 
 export default function Edit( { attributes, setAttributes } ) {
 	const blockProps = useBlockProps();
 	return (
 		<div { ...blockProps }>
+			<ServerSideRender
+				block={metadata.name}
+				attributes={attributes}
+			/>
 			<TextControl
 				value={ attributes.url }
 				onChange={ ( val ) => setAttributes( { url: val } ) }
