cab853207b9b9dd8fdf5ff8c13b0d2ac3090d93a
taako-502 <i.am.armorknight@gmail.com>
2022-06-08 21:09:49 +0900

    エディタ側のスタイルシートを調整

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => '658eb85cf77d0adcfe66');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => 'e7d781c846d25e531d79');
