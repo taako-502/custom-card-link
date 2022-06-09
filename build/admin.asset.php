fa6797796469fa89d3f38d1f837394e0c3b1a4e9
taako-502 <i.am.armorknight@gmail.com>
2022-06-09 23:00:08 +0900

    ホバー時の動作を追加

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api', 'wp-components', 'wp-element'), 'version' => '01d92e3893f54ca5e4c9');
+<?php return array('dependencies' => array('react', 'wp-api', 'wp-components', 'wp-element'), 'version' => '8b04cd0841504984b15d');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => '7db868bad52c06949295');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => '8dd5ca69203be3064a9d');
