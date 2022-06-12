1275f7778fded52e03ee929796c4a108ca2ef470
taako-502 <i.am.armorknight@gmail.com>
2022-06-12 22:18:11 +0900

    管理画面のデザイン調整

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api', 'wp-components', 'wp-element'), 'version' => 'c71871e3eff83ea3d8f7');
+<?php return array('dependencies' => array('react', 'wp-api', 'wp-components', 'wp-element'), 'version' => '356d7731cdfbcfffab83');
