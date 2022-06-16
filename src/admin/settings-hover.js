636a775cd0eb3f3ea46d3b4a42d19a59885f43f4
taako-502 <i.am.armorknight@gmail.com>
2022-06-17 00:08:59 +0900

    ホバー時の設定を外部ファイル化

---
+++ src/admin/settings-hover.js
@@ -0,0 +1,86 @@
+import { styleDisplayNone } from './visibility.js';
+
+import { RadioControl, RangeControl, ColorPicker } from '@wordpress/components';
+
+export const Hover = ( settings , setSettings ) => {
+	return (
+		<React.Fragment>
+			<h2 className="u-marign-bottom--0">カスタムリンクカードをホバーした時のデザインや動作</h2>
+			<p className="u-marign-top--4px">PCサイズとスマホサイズで共通の設定です。</p>
+			<div className='u-display--flex'>
+				<div className='u-width--50-percent'>
+					<RadioControl
+						label='ホバー時の動作'
+						help='リンクカードをホバーした際の動作'
+						selected={ settings.hoverUse }
+						options={ [
+							{ label: 'なし', value: 'none' },
+							{ label: '影を表示する', value: 'shadow' },
+						] }
+						onChange={ ( value ) => setSettings({...settings, hoverUse: value }) }
+					/>
+					<label
+						className='u-display--inline-block u-marign-top--8px'
+						style={ styleDisplayNone( settings.hoverUse ) }
+					>
+						影の色
+					</label>
+					<ColorPicker
+						color={ settings.hoverShadowColor }
+						onChange={ ( value ) => setSettings({...settings, hoverShadowColor: value }) }
+						enableAlpha
+						style={ styleDisplayNone( settings.hoverUse ) }
+					/>
+				</div>
+				<div
+					className='u-width--50-percent'
+					style={ styleDisplayNone( settings.hoverUse ) }
+				>
+					<RangeControl
+						label='ホバー時の動作時間'
+						value={ settings.hoverTransitionTime }
+						onChange={ ( value ) => setSettings({...settings, hoverTransitionTime: value }) }
+						min={ 0 }
+						max={ 1 }
+						step={ 0.1 }
+					/>
+					<RangeControl
+						label='ホバー時の高さ'
+						value={ settings.hoverTop }
+						onChange={ ( value ) => setSettings({...settings, hoverTop: value }) }
+						min={ 0 }
+						max={ 10 }
+					/>
+					<RangeControl
+						label='影の長さ（x方向）'
+						value={ settings.hoverShadowOffsetX }
+						onChange={ ( value ) => setSettings({...settings, hoverShadowOffsetX: value }) }
+						min={ -10 }
+						max={ 10 }
+					/>
+					<RangeControl
+						label='影の長さ（y方向）'
+						value={ settings.hoverShadowOffsetY }
+						onChange={ ( value ) => setSettings({...settings, hoverShadowOffsetY: value }) }
+						min={ -10 }
+						max={ 10 }
+					/>
+					<RangeControl
+						label='ぼかしの拡張・縮小'
+						value={ settings.hoverShadowBlurRadius }
+						onChange={ ( value ) => setSettings({...settings, hoverShadowBlurRadius: value }) }
+						min={ 0 }
+						max={ 10 }
+					/>
+					<RangeControl
+						label='影の拡張・縮小'
+						value={ settings.hoverShadowSpreadRadius }
+						onChange={ ( value ) => setSettings({...settings, hoverShadowSpreadRadius: value })}
+						min={ 0 }
+						max={ 10 }
+					/>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+}
--- src/admin/settings-pc.js
+++ src/admin/settings-pc.js
@@ -1,4 +1,5 @@
 import { styleDisplayNone } from './visibility.js';
+import { Hover } from './settings-hover.js';
 
 import { RadioControl, RangeControl, ColorPicker } from '@wordpress/components';
 
@@ -144,81 +145,7 @@ export const SettingsPc = ( settings , setSettings ) => {
 					/>
 				</div>
 			</div>
-			<h2>カスタムリンクカードをホバーした時のデザインや動作</h2>
-			<div className='u-display--flex'>
-				<div className='u-width--50-percent'>
-					<RadioControl
-						label='ホバー時の動作'
-						help='リンクカードをホバーした際の動作'
-						selected={ settings.hoverUse }
-						options={ [
-							{ label: 'なし', value: 'none' },
-							{ label: '影を表示する', value: 'shadow' },
-						] }
-						onChange={ ( value ) => setSettings({...settings, hoverUse: value }) }
-					/>
-					<label
-						className='u-display--inline-block u-marign-top--8px'
-						style={ styleDisplayNone( settings.hoverUse ) }
-					>
-						影の色
-					</label>
-					<ColorPicker
-						color={ settings.hoverShadowColor }
-						onChange={ ( value ) => setSettings({...settings, hoverShadowColor: value }) }
-						enableAlpha
-						style={ styleDisplayNone( settings.hoverUse ) }
-					/>
-				</div>
-				<div
-					className='u-width--50-percent'
-					style={ styleDisplayNone( settings.hoverUse ) }
-				>
-					<RangeControl
-						label='ホバー時の動作時間'
-						value={ settings.hoverTransitionTime }
-						onChange={ ( value ) => setSettings({...settings, hoverTransitionTime: value }) }
-						min={ 0 }
-						max={ 1 }
-						step={ 0.1 }
-					/>
-					<RangeControl
-						label='ホバー時の高さ'
-						value={ settings.hoverTop }
-						onChange={ ( value ) => setSettings({...settings, hoverTop: value }) }
-						min={ 0 }
-						max={ 10 }
-					/>
-					<RangeControl
-						label='影の長さ（x方向）'
-						value={ settings.hoverShadowOffsetX }
-						onChange={ ( value ) => setSettings({...settings, hoverShadowOffsetX: value }) }
-						min={ -10 }
-						max={ 10 }
-					/>
-					<RangeControl
-						label='影の長さ（y方向）'
-						value={ settings.hoverShadowOffsetY }
-						onChange={ ( value ) => setSettings({...settings, hoverShadowOffsetY: value }) }
-						min={ -10 }
-						max={ 10 }
-					/>
-					<RangeControl
-						label='ぼかしの拡張・縮小'
-						value={ settings.hoverShadowBlurRadius }
-						onChange={ ( value ) => setSettings({...settings, hoverShadowBlurRadius: value }) }
-						min={ 0 }
-						max={ 10 }
-					/>
-					<RangeControl
-						label='影の拡張・縮小'
-						value={ settings.hoverShadowSpreadRadius }
-						onChange={ ( value ) => setSettings({...settings, hoverShadowSpreadRadius: value })}
-						min={ 0 }
-						max={ 10 }
-					/>
-				</div>
-			</div>
+			{ Hover( settings , setSettings ) }
 		</React.Fragment>
 	);
 }
