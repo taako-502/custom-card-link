b4ca6c3c83302852d419c0c51c97a1cd45a62a7c
taako-502 <i.am.armorknight@gmail.com>
2022-06-16 23:57:42 +0900

    PC用の設定項目を外部ファイル化

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api', 'wp-components', 'wp-element'), 'version' => '11eabf548c85bdd88543');
+<?php return array('dependencies' => array('react', 'wp-api', 'wp-components', 'wp-element'), 'version' => 'c6b321282bc6df381726');
--- src/admin.js
+++ src/admin.js
@@ -1,15 +1,16 @@
 import './admin.scss';
+import 'react-notifications-component/dist/theme.css';
+
 import { settingNotification } from './admin/settingNotification.js';
 import { setStandardDesignCard , setStandardDesignList , setRecommendedDesign1  } from './admin/design.js';
-import { styleDisplayNone } from './admin/visibility.js';
 import { Preview } from './admin/preview.js';
+import { SettingsPc } from './admin/settings-pc.js';
 
 import { render, useState, useLayoutEffect } from '@wordpress/element';
-import { RadioControl, Button, RangeControl, ColorPicker } from '@wordpress/components';
+import { Button } from '@wordpress/components';
 import api from '@wordpress/api';
 
 import { ReactNotifications } from 'react-notifications-component'
-import 'react-notifications-component/dist/theme.css';
 
 /**
  * 管理画面
@@ -137,220 +138,7 @@ const Admin = () => {
 						</Button>
 					</div>
 					<div className='clc-admin__settings'>
-						<h2>デザイン設定</h2>
-						<div className='u-display--flex'>
-							<div className='u-width--50-percent'>
-								<RadioControl
-									label='レイアウトデザイン'
-									help='デザインのレイアウトを決めます。'
-									selected={ settings.layout }
-									options={ [
-										{ label: 'カード型', value: 'card' },
-										{ label: 'リスト型', value: 'list' },
-									] }
-									onChange={ ( value ) => setSettings({...settings, layout: value }) }
-								/>
-								<RangeControl
-									label='横幅'
-									value={ settings.width }
-									onChange={ ( value ) => setSettings({...settings, width: value }) }
-									min={ 200 }
-									max={ 1200 }
-								/>
-								<RangeControl
-									label='内側の余白'
-									value={ settings.padding }
-									onChange={ ( value ) => setSettings({...settings, padding: value }) }
-									min={ 0 }
-									max={ 40 }
-								/>
-								<RangeControl
-									label='角の丸さ（px）'
-									value={ settings.borderRadius }
-									onChange={ ( value ) => setSettings({...settings, borderRadius: value }) }
-									min={ 0 }
-									max={ 15 }
-								/>
-							</div>
-							<div className='u-width--50-percent'>
-								<RangeControl
-									label='タイトル文字サイズ'
-									value={ settings.titleFontSize }
-									onChange={ ( value ) => setSettings({...settings, titleFontSize: value }) }
-									min={ 10 }
-									max={ 30 }
-								/>
-								<RangeControl
-								label='説明文字サイズ'
-								value={ settings.descriptionFontSize }
-								onChange={ ( value ) => setSettings({...settings, descriptionFontSize: value }) }
-								min={ 10 }
-								max={ 30 }
-								/>
-								<RangeControl
-									label='タイトルの最大文字数'
-									value={ settings.titleNumOfChar }
-									onChange={ ( value ) => setSettings({...settings, titleNumOfChar: value }) }
-									min={ 10 }
-									max={ 60 }
-								/>
-								<RangeControl
-								label='説明の最大文字数'
-								value={ settings.descriptionNumOfChar }
-								onChange={ ( value ) => setSettings({...settings, descriptionNumOfChar: value }) }
-								min={ 10 }
-								max={ 200 }
-								/>
-								<RangeControl
-									label='サムネイルとタイトルおよび説明の間の余白'
-									value={ settings.gapBetweenTitleAndThumbnail }
-									onChange={ ( value ) => setSettings({...settings, gapBetweenTitleAndThumbnail: value }) }
-									min={ 0 }
-									max={ 30 }
-								/>
-								<RangeControl
-									label='タイトルと説明の間の余白'
-									value={ settings.descriptionMarginTop }
-									onChange={ ( value ) => setSettings({...settings, descriptionMarginTop: value }) }
-									min={ 0 }
-									max={ 15 }
-								/>
-							</div>
-						</div>
-						<h2>影</h2>
-						<div className='u-display--flex'>
-							<div className='u-width--50-percent'>
-								<RadioControl
-									label='影の有無'
-									selected={ settings.shadowUse }
-									options={ [
-										{ label: 'なし', value: 'none' },
-										{ label: 'あり', value: 'shadow' },
-									] }
-									onChange={ ( value ) => setSettings({...settings, shadowUse: value }) }
-								/>
-								<label
-									className='u-display--inline-block u-marign-top--8px'
-									style={ styleDisplayNone( settings.shadowUse ) }
-								>
-									影の色
-								</label>
-								<ColorPicker
-									color={ settings.shadowColor }
-									onChange={ ( value ) => setSettings({...settings, shadowColor: value }) }
-									enableAlpha
-									style={ styleDisplayNone( settings.shadowUse ) }
-								/>
-							</div>
-							<div
-								className='u-width--50-percent'
-								style={ styleDisplayNone( settings.shadowUse ) }
-							>
-								<RangeControl
-									label='影の長さ（x方向）'
-									value={ settings.shadowOffsetX }
-									onChange={ ( value ) => setSettings({...settings, shadowOffsetX: value }) }
-									min={ -10 }
-									max={ 10 }
-								/>
-								<RangeControl
-									label='影の長さ（y方向）'
-									value={ settings.shadowOffsetY }
-									onChange={ ( value ) => setSettings({...settings, shadowOffsetY: value }) }
-									min={ -10 }
-									max={ 10 }
-								/>
-								<RangeControl
-									label='ぼかしの拡張・縮小'
-									value={ settings.shadowBlurRadius }
-									onChange={ ( value ) => setSettings({...settings, shadowBlurRadius: value }) }
-									min={ 0 }
-									max={ 10 }
-								/>
-								<RangeControl
-									label='影の拡張・縮小'
-									value={ settings.shadowSpreadRadius }
-									onChange={ ( value ) => setSettings({...settings, shadowSpreadRadius: value }) }
-									min={ 0 }
-									max={ 10 }
-								/>
-							</div>
-						</div>
-						<h2>カスタムリンクカードをホバーした時のデザインや動作</h2>
-						<div className='u-display--flex'>
-							<div className='u-width--50-percent'>
-								<RadioControl
-									label='ホバー時の動作'
-									help='リンクカードをホバーした際の動作'
-									selected={ settings.hoverUse }
-									options={ [
-										{ label: 'なし', value: 'none' },
-										{ label: '影を表示する', value: 'shadow' },
-									] }
-									onChange={ ( value ) => setSettings({...settings, hoverUse: value }) }
-								/>
-								<label
-									className='u-display--inline-block u-marign-top--8px'
-									style={ styleDisplayNone( settings.hoverUse ) }
-								>
-									影の色
-								</label>
-								<ColorPicker
-									color={ settings.hoverShadowColor }
-									onChange={ ( value ) => setSettings({...settings, hoverShadowColor: value }) }
-									enableAlpha
-									style={ styleDisplayNone( settings.hoverUse ) }
-								/>
-							</div>
-							<div
-								className='u-width--50-percent'
-								style={ styleDisplayNone( settings.hoverUse ) }
-							>
-								<RangeControl
-									label='ホバー時の動作時間'
-									value={ settings.hoverTransitionTime }
-									onChange={ ( value ) => setSettings({...settings, hoverTransitionTime: value }) }
-									min={ 0 }
-									max={ 1 }
-									step={ 0.1 }
-								/>
-								<RangeControl
-									label='ホバー時の高さ'
-									value={ settings.hoverTop }
-									onChange={ ( value ) => setSettings({...settings, hoverTop: value }) }
-									min={ 0 }
-									max={ 10 }
-								/>
-								<RangeControl
-									label='影の長さ（x方向）'
-									value={ settings.hoverShadowOffsetX }
-									onChange={ ( value ) => setSettings({...settings, hoverShadowOffsetX: value }) }
-									min={ -10 }
-									max={ 10 }
-								/>
-								<RangeControl
-									label='影の長さ（y方向）'
-									value={ settings.hoverShadowOffsetY }
-									onChange={ ( value ) => setSettings({...settings, hoverShadowOffsetY: value }) }
-									min={ -10 }
-									max={ 10 }
-								/>
-								<RangeControl
-									label='ぼかしの拡張・縮小'
-									value={ settings.hoverShadowBlurRadius }
-									onChange={ ( value ) => setSettings({...settings, hoverShadowBlurRadius: value }) }
-									min={ 0 }
-									max={ 10 }
-								/>
-								<RangeControl
-									label='影の拡張・縮小'
-									value={ settings.hoverShadowSpreadRadius }
-									onChange={ ( value ) => setSettings({...settings, hoverShadowSpreadRadius: value })}
-									min={ 0 }
-									max={ 10 }
-								/>
-							</div>
-						</div>
+						{ SettingsPc( settings , setSettings ) }
 					</div>
 				</div>
 			</div>
