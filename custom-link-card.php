fb62e667a902c2a4763bb66823ec017a2b76dfe8
taako-502 <i.am.armorknight@gmail.com>
2022-06-20 00:51:08 +0900

    refactor: スタイルシートを外部ファイル化

--- custom-link-card.php
+++ custom-link-card.php
@@ -16,6 +16,7 @@ const DB_NAME      = 'custom_link_card_settings';
 require_once __DIR__ .'/library/Get_OGP_InWP/get_ogp_inwp.php';
 require_once __DIR__ .'/library/plugin-update-checker/plugin-update-checker.php';
 require_once __DIR__ .'/functions/rest_api.php';
+require_once __DIR__ .'/functions/style.php';
 
 /**
  * プラグインアップデーター
@@ -260,39 +261,3 @@ function getDescription($id, $len){
 	$description = preg_replace('/\[.*\]/','',$description);
 	return mb_substr($description, 0, $len);
 }
-
-/**
- * 動的スタイルシート
- * @return string
- */
-function dynamic_styles(){
-	$css  = '';
-	$css .= '<style type="text/css">';
-	$css .= '  .clc {';
-	$css .= '    box-shadow: '.get_setting('shadow_offset_x').'px '.get_setting('shadow_offset_y').'px '
-		.get_setting('shadow_blur_radius').'px '.get_setting('shadow_spread_radius').'px '
-		.get_setting('shadow_color').';';
-	$css .= '  }';
-	$css .= '  .clc--hover-shadow:hover {';
-	$css .= '    box-shadow: '.get_setting('hover_shadow_offset_x').'px '.get_setting('hover_shadow_offset_y').'px '
-		.get_setting('hover_shadow_blur_radius').'px '.get_setting('hover_shadow_spread_radius').'px '
-		.get_setting('hover_shadow_color').';';
-	$css .= '  }';
-	//レスポンシブ
-	$css .= '@media screen and (max-width: '.get_setting('breakpoint').'px) {';
-	$css .= '  .clc {';
-	$css .= '    box-shadow: '.get_setting('shadow_offset_x_sp').'px '.get_setting('shadow_offset_y_sp').'px '
-		.get_setting('shadow_blur_radius_sp').'px '.get_setting('shadow_spread_radius_sp').'px '
-		.get_setting('shadow_color_sp').';';
-	$css .= '  }';
-	$css .= '}';
-	$css .= '</style>';
-	return $css;
-}
-
-/**
- * ヘッダーにCSSを挿入
- */
-add_action('wp_head', function() {
-	echo dynamic_styles();
-});
---
+++ functions/style.php
@@ -0,0 +1,40 @@
+<?php
+namespace Clc_Plugin\functions\rest_api;
+
+use function Clc_Plugin\get_setting;
+
+/**
+ * 動的スタイルシート
+ * @return string
+ */
+function dynamic_styles(){
+	$css  = '';
+	$css .= '<style type="text/css">';
+	$css .= '  .clc {';
+	$css .= '    box-shadow: '.get_setting('shadow_offset_x').'px '.get_setting('shadow_offset_y').'px '
+		.get_setting('shadow_blur_radius').'px '.get_setting('shadow_spread_radius').'px '
+		.get_setting('shadow_color').';';
+	$css .= '  }';
+	$css .= '  .clc--hover-shadow:hover {';
+	$css .= '    box-shadow: '.get_setting('hover_shadow_offset_x').'px '.get_setting('hover_shadow_offset_y').'px '
+		.get_setting('hover_shadow_blur_radius').'px '.get_setting('hover_shadow_spread_radius').'px '
+		.get_setting('hover_shadow_color').';';
+	$css .= '  }';
+	//レスポンシブ
+	$css .= '@media screen and (max-width: '.get_setting('breakpoint').'px) {';
+	$css .= '  .clc {';
+	$css .= '    box-shadow: '.get_setting('shadow_offset_x_sp').'px '.get_setting('shadow_offset_y_sp').'px '
+		.get_setting('shadow_blur_radius_sp').'px '.get_setting('shadow_spread_radius_sp').'px '
+		.get_setting('shadow_color_sp').';';
+	$css .= '  }';
+	$css .= '}';
+	$css .= '</style>';
+	return $css;
+}
+
+/**
+ * ヘッダーにCSSを挿入
+ */
+add_action('wp_head', function() {
+	echo dynamic_styles();
+});
