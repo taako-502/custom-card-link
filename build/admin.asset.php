ebf7c80de2058333d6adf4c8308f33190cda3890
taako-502 <i.am.armorknight@gmail.com>
2022-06-26 17:17:35 +0900

    fix: ディスクリプションの上の余白を調整する機能が動作するよう修正

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api', 'wp-components', 'wp-element'), 'version' => 'd4f9b10b5a77d86b46fb');
+<?php return array('dependencies' => array('react', 'wp-api', 'wp-components', 'wp-element'), 'version' => 'f259c3da6177519b6ccc');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => 'fb8b344b26d7c735bd59');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => '4f9cdf552af1a0a09e81');
