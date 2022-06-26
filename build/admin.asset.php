14d5e55e79a4b8f36d02ebf13dfbaede327564bf
taako-502 <i.am.armorknight@gmail.com>
2022-06-26 17:23:41 +0900

    fix: サムネイルがカードリンクをはみ出す問題を修正

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api', 'wp-components', 'wp-element'), 'version' => 'f259c3da6177519b6ccc');
+<?php return array('dependencies' => array('react', 'wp-api', 'wp-components', 'wp-element'), 'version' => '3ff9558915f4c49ebf27');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => '4f9cdf552af1a0a09e81');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-server-side-render'), 'version' => '94b995962ce8ae1260d7');
