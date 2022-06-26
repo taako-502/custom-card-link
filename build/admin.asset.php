9735de4d4eed1576ab035eea0bfea037ef26283d
taako-502 <i.am.armorknight@gmail.com>
2022-06-26 19:26:41 +0900

    style: 見出し下のデザインの調整

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api', 'wp-components', 'wp-element'), 'version' => 'df80db422678b870b162');
+<?php return array('dependencies' => array('react', 'wp-api', 'wp-components', 'wp-element'), 'version' => 'a951e84f97a3c1d65c5d');
