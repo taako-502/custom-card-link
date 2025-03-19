0e48dcb0530a5886cf46f1d14be780c733f0dbab
taako-502 <yamamoto.takao502@gmail.com>
2025-03-19 20:52:08 +0900

    fixx

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '252db134fc55c5d9f5ce');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '022a9f7759fcaa1e04bf');
--- src/admin.js
+++ src/admin.js
@@ -37,107 +37,120 @@ const Admin = () => {
 				) {
 					setStandardDesignCard( setSettings );
 				} else {
-					setSettings( {
-						...settings,
-						layout: response.custom_card_link_settings.layout,
-						maxWidth: response.custom_card_link_settings.max_width,
-						padding: response.custom_card_link_settings.padding,
-						borderRadius:
-							response.custom_card_link_settings.border_radius,
-						titleFontSize:
-							response.custom_card_link_settings.title_font_size,
-						descriptionFontSize:
-							response.custom_card_link_settings
-								.description_font_size,
-						titleNumOfChar:
-							response.custom_card_link_settings
-								.title_num_of_char,
-						descriptionNumOfChar:
-							response.custom_card_link_settings
-								.description_num_of_char,
-						gapBetweenTitleAndThumbnail:
-							response.custom_card_link_settings
-								.gap_between_title_and_thumbnail,
-						descriptionMarginTop:
-							response.custom_card_link_settings
-								.description_margin_top,
-						shadowUse:
-							response.custom_card_link_settings.shadow_use,
-						shadowOffsetX:
-							response.custom_card_link_settings.shadow_offset_x,
-						shadowOffsetY:
-							response.custom_card_link_settings.shadow_offset_y,
-						shadowBlurRadius:
-							response.custom_card_link_settings
-								.shadow_blur_radius,
-						shadowSpreadRadius:
-							response.custom_card_link_settings
-								.shadow_spread_radius,
-						shadowColor:
-							response.custom_card_link_settings.shadow_color,
-						breakpoint:
-							response.custom_card_link_settings.breakpoint,
-						layoutSp: response.custom_card_link_settings.layout_sp,
-						maxWidthSp:
-							response.custom_card_link_settings.max_width_sp,
-						paddingSp:
-							response.custom_card_link_settings.padding_sp,
-						borderRadiusSp:
-							response.custom_card_link_settings.border_radius_sp,
-						titleFontSizeSp:
-							response.custom_card_link_settings
-								.title_font_size_sp,
-						descriptionFontSizeSp:
-							response.custom_card_link_settings
-								.description_font_size_sp,
-						titleNumOfCharSp:
-							response.custom_card_link_settings
-								.title_num_of_char_sp,
-						descriptionNumOfCharSp:
-							response.custom_card_link_settings
-								.description_num_of_char_sp,
-						gapBetweenTitleAndThumbnailSp:
-							response.custom_card_link_settings
-								.gap_between_title_and_thumbnail_sp,
-						descriptionMarginTopSp:
-							response.custom_card_link_settings
-								.description_margin_top_sp,
-						shadowUseSp:
-							response.custom_card_link_settings.shadow_use_sp,
-						shadowOffsetXSp:
-							response.custom_card_link_settings
-								.shadow_offset_x_sp,
-						shadowOffsetYSp:
-							response.custom_card_link_settings
-								.shadow_offset_y_sp,
-						shadowBlurRadiusSp:
-							response.custom_card_link_settings
-								.shadow_blur_radius_sp,
-						shadowSpreadRadiusSp:
-							response.custom_card_link_settings
-								.shadow_spread_radius_sp,
-						shadowColorSp:
-							response.custom_card_link_settings.shadow_color_sp,
-						hoverUse: response.custom_card_link_settings.hover_use,
-						hoverTop: response.custom_card_link_settings.hover_top,
-						hoverTransitionTime:
-							response.custom_card_link_settings
-								.hover_transition_time,
-						hoverShadowOffsetX:
-							response.custom_card_link_settings
-								.hover_shadow_offset_x,
-						hoverShadowOffsetY:
-							response.custom_card_link_settings
-								.hover_shadow_offset_y,
-						hoverShadowBlurRadius:
-							response.custom_card_link_settings
-								.hover_shadow_blur_radius,
-						hoverShadowSpreadRadius:
-							response.custom_card_link_settings
-								.hover_shadow_spread_radius,
-						hoverShadowColor:
-							response.custom_card_link_settings
-								.hover_shadow_color,
+					setSettings( ( prevSettings ) => {
+						return {
+							...prevSettings,
+							layout: response.custom_card_link_settings.layout,
+							maxWidth:
+								response.custom_card_link_settings.max_width,
+							padding: response.custom_card_link_settings.padding,
+							borderRadius:
+								response.custom_card_link_settings
+									.border_radius,
+							titleFontSize:
+								response.custom_card_link_settings
+									.title_font_size,
+							descriptionFontSize:
+								response.custom_card_link_settings
+									.description_font_size,
+							titleNumOfChar:
+								response.custom_card_link_settings
+									.title_num_of_char,
+							descriptionNumOfChar:
+								response.custom_card_link_settings
+									.description_num_of_char,
+							gapBetweenTitleAndThumbnail:
+								response.custom_card_link_settings
+									.gap_between_title_and_thumbnail,
+							descriptionMarginTop:
+								response.custom_card_link_settings
+									.description_margin_top,
+							shadowUse:
+								response.custom_card_link_settings.shadow_use,
+							shadowOffsetX:
+								response.custom_card_link_settings
+									.shadow_offset_x,
+							shadowOffsetY:
+								response.custom_card_link_settings
+									.shadow_offset_y,
+							shadowBlurRadius:
+								response.custom_card_link_settings
+									.shadow_blur_radius,
+							shadowSpreadRadius:
+								response.custom_card_link_settings
+									.shadow_spread_radius,
+							shadowColor:
+								response.custom_card_link_settings.shadow_color,
+							breakpoint:
+								response.custom_card_link_settings.breakpoint,
+							layoutSp:
+								response.custom_card_link_settings.layout_sp,
+							maxWidthSp:
+								response.custom_card_link_settings.max_width_sp,
+							paddingSp:
+								response.custom_card_link_settings.padding_sp,
+							borderRadiusSp:
+								response.custom_card_link_settings
+									.border_radius_sp,
+							titleFontSizeSp:
+								response.custom_card_link_settings
+									.title_font_size_sp,
+							descriptionFontSizeSp:
+								response.custom_card_link_settings
+									.description_font_size_sp,
+							titleNumOfCharSp:
+								response.custom_card_link_settings
+									.title_num_of_char_sp,
+							descriptionNumOfCharSp:
+								response.custom_card_link_settings
+									.description_num_of_char_sp,
+							gapBetweenTitleAndThumbnailSp:
+								response.custom_card_link_settings
+									.gap_between_title_and_thumbnail_sp,
+							descriptionMarginTopSp:
+								response.custom_card_link_settings
+									.description_margin_top_sp,
+							shadowUseSp:
+								response.custom_card_link_settings
+									.shadow_use_sp,
+							shadowOffsetXSp:
+								response.custom_card_link_settings
+									.shadow_offset_x_sp,
+							shadowOffsetYSp:
+								response.custom_card_link_settings
+									.shadow_offset_y_sp,
+							shadowBlurRadiusSp:
+								response.custom_card_link_settings
+									.shadow_blur_radius_sp,
+							shadowSpreadRadiusSp:
+								response.custom_card_link_settings
+									.shadow_spread_radius_sp,
+							shadowColorSp:
+								response.custom_card_link_settings
+									.shadow_color_sp,
+							hoverUse:
+								response.custom_card_link_settings.hover_use,
+							hoverTop:
+								response.custom_card_link_settings.hover_top,
+							hoverTransitionTime:
+								response.custom_card_link_settings
+									.hover_transition_time,
+							hoverShadowOffsetX:
+								response.custom_card_link_settings
+									.hover_shadow_offset_x,
+							hoverShadowOffsetY:
+								response.custom_card_link_settings
+									.hover_shadow_offset_y,
+							hoverShadowBlurRadius:
+								response.custom_card_link_settings
+									.hover_shadow_blur_radius,
+							hoverShadowSpreadRadius:
+								response.custom_card_link_settings
+									.hover_shadow_spread_radius,
+							hoverShadowColor:
+								response.custom_card_link_settings
+									.hover_shadow_color,
+						};
 					} );
 				}
 			} );
@@ -202,18 +215,21 @@ const Admin = () => {
 		<React.Fragment>
 			<ReactNotifications />
 			<div className="ccl-admin">
-				<h1>{__('Design settings', 'ccl-plugin' )}</h1>
+				<h1>{ __( 'Design settings', 'ccl-plugin' ) }</h1>
 				<div className="ccl-admin__wrap">
 					<div className="ccl-admin__preview">
 						<div className="ccl-admin__info">
-							<h2>{__('Preview', 'ccl-plugin' )}</h2>
+							<h2>{ __( 'Preview', 'ccl-plugin' ) }</h2>
 							<ToggleControl
 								label={
-									 ! mediaSize
-									 ? __('Computer', 'ccl-plugin' )
-									 : __('Smartphone', 'ccl-plugin' )
+									! mediaSize
+										? __( 'Computer', 'ccl-plugin' )
+										: __( 'Smartphone', 'ccl-plugin' )
 								}
-								help={__('Setting up a smartphone', 'ccl-plugin' ) }
+								help={ __(
+									'Setting up a smartphone',
+									'ccl-plugin'
+								) }
 								checked={ mediaSize }
 								onChange={ () => {
 									setmediaSize( ( state ) => ! state );
