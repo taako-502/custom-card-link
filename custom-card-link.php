38752881d0ac4fc3a54e784f0ab3414a30fbdf62
taako-502 <yamamoto.takao502@gmail.com>
2025-03-20 16:30:00 +0900

    version up

--- custom-card-link.php
+++ custom-card-link.php
@@ -5,7 +5,7 @@ Namespace Ccl_Plugin;
 Plugin Name: Custom Card Link
 Plugin URI: https://github.com/taako-502/custom-card-link
 Description: 外部リンクを表示するGutenbergブロック
-Version: 1.0.16
+Version: 1.0.17
 Author: takao502
 Author URI: https://github.com/taako-502
 Text Domain: ccl-plugin
