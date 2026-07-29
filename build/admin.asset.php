aa5459e9b179475a882dffc5ab7f6383ac2f2547
taako-502 <yamamoto.takao502@gmail.com>
2026-07-29 23:35:56 +0900

    package json version up

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '022a9f7759fcaa1e04bf');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8eacf3da2c120fd56a0a');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-server-side-render'), 'version' => '458145b6c9443ff4dbc6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-server-side-render'), 'version' => '69a228ce3c7a69f000d2');
