95c8b6f9e7f54e56263f2ee12f81a6b9a73778db
Takao <36348377+taako-502@users.noreply.github.com>
2026-09-14 03:01:57 +0900

    Align plugin description with WordPress.org listing

--- custom-card-link.php
+++ custom-card-link.php
@@ -4,7 +4,7 @@ Namespace Ccl_Plugin;
 /*
 Plugin Name: Custom Card Link
 Plugin URI: https://github.com/taako-502/custom-card-link
-Description: 外部リンクを表示するGutenbergブロック
+Description: Create customizable blog cards and link previews for internal and external URLs with OGP metadata in the Gutenberg block editor.
 Version: 1.1.7
 Author: takao502
 Author URI: https://github.com/taako-502
